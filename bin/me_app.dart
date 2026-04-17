163f970f6a7a13a5618f39507c3eb47472f2ea83
gabrieltoninatto <gabrieltoninatto1@hotmail.com>
2026-04-17 15:21:50 -0300

    atualiza a função main para solicitar nome e idade do usuário

--- bin/me_app.dart
+++ bin/me_app.dart
@@ -1,7 +1,15 @@
 import 'dart:io';
 
 void main () {
-  print("Olá, me chamo Dart. Qual é o seu nome?");
-  var entrada = stdin.readLineSync();
-  print("Muito prazer, $entrada. Vamos fazer vários programas juntos!");
+  //print("Olá, me chamo Dart. Qual é o seu nome?");
+  //var entrada = stdin.readLineSync();
+  //print("Muito prazer, $entrada. Vamos fazer vários programas juntos!");
+
+  print("Olá! Qual o seu nome?");
+  String? nome = stdin.readLineSync();
+
+  print("Qual a sua idade?");
+  String? idade = stdin.readLineSync();
+
+  print("Olá $nome, você tem $idade anos!");
 }
