ec7914056b48eb65ee8975b3f9f51a0dfa308e32
gabrieltoninatto <gabrieltoninatto1@hotmail.com>
2026-04-17 15:13:52 -0300

    substitui mensagem de saudação "Hello World" por uma interação personalizada com o usuário

--- bin/me_app.dart
+++ bin/me_app.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
 
 void main () {
-  print("Hello World");
+  print("Olá, me chamo Dart. Qual é o seu nome?");
+  var entrada = stdin.readLineSync();
+  print("Muito prazer, $entrada. Vamos fazer vários programas juntos!");
 }
